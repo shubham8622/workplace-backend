cbbf035f061de30849536c7e91718c024fbc5052
shubham chandel <shubhamchandel127.0.0.1@gmail.com>
2022-12-04 14:04:44 +0530

    added new route

--- src/routes/index.js
+++ src/routes/index.js
@@ -41,6 +41,12 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cors());
 app.use('/uploads',express.static('uploads'));
+app.get("/",(re,res)=>{
+    res.status(200).json({
+        success:true,
+        message:"Hello"
+    })
+})
 app.post("/register", async (req,res)=>{
     
     const {fName,lName,who,gender,eMail,password} = req.body;
